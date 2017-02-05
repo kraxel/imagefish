f1b9949e7622014e6186bbc191fcb4d49a866772
Gerd Hoffmann <kraxel@redhat.com>
2017-02-05 14:41:25 +0100

    tweak f25, add rhel7

--- RHEL73-efi.sh
+++ RHEL73-efi.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 arch="$(uname -m)"
-tar="fedora-25-efi-${arch}.tar.gz"
-img="fedora-25-efi-${arch}.qcow2"
+tar="rhel-73-efi-${arch}.tar.gz"
+img="rhel-73-efi-${arch}.qcow2"
 set -ex
 rm -f "$tar" "$img"
 scripts/install-redhat.sh			\
-	--config	repos/fedora-25.repo	\
+	--config	repos/rhel-73.repo	\
 	--tar		"$tar"			\
 	--packages	"grub2-efi shim"	\
-	--dnf 
+	--yum
 scripts/tar-to-image.sh				\
 	--tar		"$tar"			\
 	--image		"$img"
