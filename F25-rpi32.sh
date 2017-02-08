a203c938475db67d4a0e055a37e6007785d06c6b
Gerd Hoffmann <kraxel@redhat.com>
2017-02-08 17:51:34 +0100

    need kernel-modules for physical hardware

--- F25-rpi32.sh
+++ F25-rpi32.sh
@@ -3,11 +3,12 @@
 name="fedora-25-rpi2"
 repo="repos/fedora-25.repo"
 rpms="bcm283x-firmware uboot-images-armv7 extlinux-bootloader dracut-config-generic"
+krnl="kernel kernel-modules"
 
 arch="$(uname -m)"
 tar="${name}-${arch}.tar.gz"
 img="${name}-${arch}.raw"
 set -ex
 rm -f "$tar" "$img"
-scripts/install-redhat.sh --config "$repo" --tar "$tar" --packages "$rpms" --dnf 
+scripts/install-redhat.sh --config "$repo" --tar "$tar" --packages "$rpms" --kernel "$krnl" --dnf 
 scripts/tar-to-image.sh --tar "$tar" --image "$img" --rpi32
