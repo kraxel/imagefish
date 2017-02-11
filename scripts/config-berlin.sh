f2ba9a72e70bdbe28577bb6232a8aa37d1abcc0c
Gerd Hoffmann <kraxel@redhat.com>
2017-02-11 13:15:57 +0100

    add scripts/config-berlin.sh

---
+++ scripts/config-berlin.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+image="$1"
+passwd="$2"
+if test "$image" = "" -o "$passwd" = ""; then
+	echo "usage: $0 <image> <root-password>"
+	exit 1
+fi
+set -ex
+virt-customize -a "$image"		\
+	--timezone "Europe/Berlin"	\
+	--edit "/etc/vconsole.conf:s/KEYMAP=.*/KEYMAP=de-nodeadkeys/"
