d988876617f99c884b40e44fe3f88429fd481faf
Gerd Hoffmann <kraxel@redhat.com>
2017-02-28 16:34:13 +0100

    add F25-efi-systemd.sh

---
+++ F25-efi-systemd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+name="fedora-25-efi-systemd"
+repo="repos/fedora-25.repo"
+rpms="-grubby -dracut-config-rescue"
+
+arch="$(uname -m)"
+tar="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.tar.gz"
+img="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.qcow2"
+
+echo ""
+echo "###"
+echo "### $name"
+echo "###"
+
+set -ex
+rm -f "$tar" "$img"
+scripts/install-redhat.sh --config "$repo" --tar "$tar" --packages "$rpms" --dnf
+scripts/tar-to-image.sh --tar "$tar" --image "$img" --efi-systemd
