94dd13c673bb156e1f148b19bb8314ee1c998364
Gerd Hoffmann <kraxel@redhat.com>
2017-03-01 20:52:27 +0100

    cubietruck: comment workaround

--- run-all.sh
+++ run-all.sh
@@ -5,8 +5,8 @@ BASE="$(dirname $0)"
 case "$(hostname --short)" in
 cubietruck)
 	# quirks
-	export SUPERMIN_KERNEL=/boot/vmlinuz-4.9.5-200.fc25.armv7hl+lpae
-	export SUPERMIN_KERNEL_VERSION=4.9.5-200.fc25.armv7hl+lpae
+#	export SUPERMIN_KERNEL=/boot/vmlinuz-4.9.5-200.fc25.armv7hl+lpae
+#	export SUPERMIN_KERNEL_VERSION=4.9.5-200.fc25.armv7hl+lpae
 	rm -rf /var/tmp/.guestfs-500
 
 	# rebuild images
