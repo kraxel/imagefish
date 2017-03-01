eeddfef766d5574385546bba84b09306df035aab
Gerd Hoffmann <kraxel@redhat.com>
2017-03-01 20:51:49 +0100

    grub2 for armv7

--- F25-efi-armv7.sh
+++ F25-efi-armv7.sh
@@ -16,6 +16,6 @@ echo "###"
 set -ex
 rm -f "$tar" "$img"
 scripts/install-redhat.sh --config "$repo" --tar "$tar" --packages "$rpms" --dnf
-scripts/tar-to-image.sh --tar "$tar" --image "$img" --efi
+scripts/tar-to-image.sh --tar "$tar" --image "$img" --efi-grub2
 scripts/config-systemd-network.sh "$img"
 scripts/config-kraxel-repo.sh "$img"
