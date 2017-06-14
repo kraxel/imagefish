4e768395925f6972d47b7160f7b12d8541b1b381
Gerd Hoffmann <kraxel@redhat.com>
2017-06-14 17:05:36 +0200

    add clover wrapper script

---
+++ clover.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+src="/vmdisk/hdd/pool-iso"
+dst="/vmdisk/hdd/pool-disk"
+
+iso="$(ls -t $src/Clover-*-X64.iso | head -1)"
+img="${dst}${iso#$src}"
+img="${img%.iso}.qcow2"
+
+# rebuild clover image
+set -x
+rm -f "$img"
+scripts/clover-image.sh --iso "$iso" --img "$img" --cfg "clover/config.plist"
