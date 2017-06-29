8cd4f172ddcf0714aebee7d14bc9c7b1559f1849
Gerd Hoffmann <kraxel@redhat.com>
2017-06-29 18:08:13 +0200

    zap initrd before kernel-install run

--- scripts/tar-to-image.sh
+++ scripts/tar-to-image.sh
@@ -282,6 +282,7 @@ function fish_systemd_boot() {
 	echo "### init systemd-boot"
 	fish mkdir-p /etc/kernel
 	fish write /etc/kernel/cmdline "ro root=${rootfs} ${console}"
+	fish glob rm-f "/boot/*/*/initrd"
 	fish command "bootctl --no-variables install"
 	fish command "kernel-install add ${kver} /lib/modules/${kver}/vmlinuz"
 	fish command "sed -i -e '/timeout/s/^#//' /boot/loader/loader.conf"
