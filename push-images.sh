cf7d8ad8ecbe6122d064d574913ad029492958da
Gerd Hoffmann <kraxel@redhat.com>
2017-07-06 15:07:52 +0200

    rework push-images

--- push-images.sh
+++ push-images.sh
@@ -2,24 +2,32 @@
 
 dest="$HOME/repo/images-testing"
 
-if test -d "$dest"; then
-	echo
-	echo "# rsync uncompressed images"
-	rsync --verbose --progress		\
-		${IMAGEFISH_DESTDIR-.}/*.raw	\
-		${IMAGEFISH_DESTDIR-.}/*.qcow2	\
-		$dest
-
-	echo
-	echo "# compress images"
-	rm -f ${IMAGEFISH_DESTDIR-.}/*.xz
-	xz --verbose --keep			\
-		${IMAGEFISH_DESTDIR-.}/*.raw	\
-		${IMAGEFISH_DESTDIR-.}/*.qcow2
-
-	echo
-	echo "# rsync compressed images"
-	rsync --verbose --progress		\
-		${IMAGEFISH_DESTDIR-.}/*.xz	\
-		$dest
+if test ! -d "$dest"; then
+	echo "# not found: $dest"
+	exit
 fi
+
+images=$(ls	${IMAGEFISH_DESTDIR-.}/*.raw	\
+		${IMAGEFISH_DESTDIR-.}/*.qcow2	\
+		2>/dev/null)
+count=$(echo $images | wc -w)
+
+if test "$count" = "0"; then
+	echo "# no images"
+	exit
+fi
+
+echo
+echo "# rsync $count uncompressed images"
+rsync --verbose --progress $images $dest
+
+echo
+echo "# compress $count images"
+rm -f ${IMAGEFISH_DESTDIR-.}/*.xz
+xz --verbose --keep $images
+
+echo
+echo "# rsync compressed images"
+rsync --verbose --progress		\
+	${IMAGEFISH_DESTDIR-.}/*.xz	\
+	$dest
