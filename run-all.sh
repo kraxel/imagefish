bdf92574cb0502d1ed4ec8ed47219867dd989312
Gerd Hoffmann <kraxel@redhat.com>
2017-07-07 11:38:19 +0200

    tweak run-all

--- run-all.sh
+++ run-all.sh
@@ -12,18 +12,19 @@ arm-b64)
 	# rebuild images
 	./Fedora-rpi64-fedora.sh	26	|| exit 1
 	./Fedora-rpi64-kraxel.sh	26	|| exit 1
-#	./Fedora-efi-grub2.sh 		25	|| exit 1
+	./Fedora-efi-grub2.sh 		26	|| exit 1
 	;;
 sirius)
 	# rebuild images
 	export IMAGEFISH_DESTDIR="/vmdisk/hdd/imagefish"
+
+	./Fedora-efi-grub2.sh		26	|| exit 1
+	./Fedora-efi-systemd.sh		26	|| exit 1
+	linux32 ./Fedora-efi-systemd.sh	26	|| exit 1
+
 	./RHEL73-efi.sh				|| exit 1
 	./RHEL74-efi.sh				|| exit 1
 	./CentOS7-efi.sh			|| exit 1
-
-#	linux32 ./Fedora-efi-systemd.sh	25	|| exit 1
-	./Fedora-efi-grub2.sh		26	|| exit 1
-	./Fedora-efi-systemd.sh		26	|| exit 1
 	;;
 *)
 	echo "unknown host, don't know what to do"
