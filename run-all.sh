8da90a01cb7a2c3299fb4540d3b0d93a68e7127b
Gerd Hoffmann <kraxel@redhat.com>
2019-06-05 13:35:06 +0200

    fedora 30

--- run-all.sh
+++ run-all.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
 BASE="$(dirname $0)"
+FVER="30"
 
 case "$(hostname --short)" in
 arm-b32 | cubietruck)
 	# rebuild images
-#	./Fedora-rpi32.sh		29	|| exit 1
-	./Fedora-efi-armv7.sh		29	|| exit 1
-#	./Fedora-efi-grub2.sh		29	|| exit 1
+#	./Fedora-rpi32.sh		$FVER	|| exit 1
+	./Fedora-efi-armv7.sh		$FVER	|| exit 1
+#	./Fedora-efi-grub2.sh		$FVER	|| exit 1
 	;;
 arm-b64)
 	# rebuild images
-#	./Fedora-rpi64.sh		29	|| exit 1
-	./Fedora-efi-grub2.sh 		29	|| exit 1
-#	./Fedora-efi-systemd.sh 	29	|| exit 1
+#	./Fedora-rpi64.sh		$FVER	|| exit 1
+	./Fedora-efi-grub2.sh 		$FVER	|| exit 1
+#	./Fedora-efi-systemd.sh 	$FVER	|| exit 1
 	;;
 sirius)
 	# rebuild images
 	export IMAGEFISH_DESTDIR="/vmdisk/hdd/imagefish"
 
-	./Fedora-efi-grub2.sh		29	|| exit 1
-	./Fedora-efi-systemd.sh		29	|| exit 1
-	linux32 ./Fedora-efi-grub2.sh	29	|| exit 1
-	linux32 ./Fedora-efi-systemd.sh	29	|| exit 1
+	./Fedora-efi-grub2.sh		$FVER	|| exit 1
+	./Fedora-efi-systemd.sh		$FVER	|| exit 1
+	linux32 ./Fedora-efi-grub2.sh	$FVER	|| exit 1
+	linux32 ./Fedora-efi-systemd.sh	$FVER	|| exit 1
 
 	./RHEL75-efi.sh				|| exit 1
 	./RHEL76-efi.sh				|| exit 1
