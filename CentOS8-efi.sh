1e5d47074aa0fc116fa48b67defda73b7ea2a4b8
Gerd Hoffmann <kraxel@redhat.com>
2019-12-03 19:26:09 +0100

    add centos8

---
+++ CentOS8-efi.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+name="centos-8-efi"
+repo="repos/centos-8.repo"
+rpms="grub2-efi grub2-pc shim -grubby -dracut-config-rescue dracut-config-generic"
+
+arch="$(uname -m)"
+tar="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.tar.gz"
+img="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.qcow2"
+
+echo ""
+echo "###"
+echo "### $name ($arch)"
+echo "###"
+echo "### $rpms"
+echo "###"
+
+set -ex
+rm -f "$tar" "$img"
+scripts/install-redhat.sh --config "$repo" --tar "$tar" --packages "$rpms" --yum
+scripts/tar-to-image.sh --tar "$tar" --image "$img" --efi-grub2
