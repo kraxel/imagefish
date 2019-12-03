6eb0aaba3c2e63d48728198c48bce54082f463d6
Gerd Hoffmann <kraxel@redhat.com>
2019-12-03 19:21:34 +0100

    armv7 fixup

--- Fedora-efi-armv7.sh
+++ Fedora-efi-armv7.sh
@@ -3,9 +3,9 @@
 vers="${1-30}"
 name="fedora-${vers}-efi-grub2"
 repo="repos/fedora-${vers}-$(sh basearch.sh).repo"
-rpms="grub2-efi -dracut-config-rescue dracut-config-generic"
+rpms="grub2-efi grubby-deprecated -dracut-config-rescue dracut-config-generic"
 
-arch="$(uname -m)"
+arch="$(sh basearch.sh)"
 tar="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.tar.gz"
 img="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.qcow2"
 
@@ -18,4 +18,4 @@ set -ex
 rm -f "$tar" "$img"
 scripts/install-redhat.sh --config "$repo" --tar "$tar" --packages "$rpms" --dnf
 scripts/tar-to-image.sh --tar "$tar" --image "$img" --efi-grub2
-scripts/config-kraxel-repo.sh "$img"
+#scripts/config-kraxel-repo.sh "$img"
