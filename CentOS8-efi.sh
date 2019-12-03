a7457982d9f9bfa1e00f007e040d30ae52406870
Gerd Hoffmann <kraxel@redhat.com>
2019-12-03 19:27:49 +0100

    tweak centos8

--- CentOS8-efi.sh
+++ CentOS8-efi.sh
@@ -2,7 +2,11 @@
 
 name="centos-8-efi"
 repo="repos/centos-8.repo"
-rpms="grub2-efi grub2-pc shim -grubby -dracut-config-rescue dracut-config-generic"
+rpms="shim -grubby -dracut-config-rescue dracut-config-generic"
+
+rpms_x86_64="shim grub2-efi grub2-pc"
+rpms_aarch64="shim grub2-efi"
+eval "rpms=\"\$rpms \$rpms_$(sh basearch.sh)\""
 
 arch="$(uname -m)"
 tar="${IMAGEFISH_DESTDIR-.}/${name}-${arch}.tar.gz"
