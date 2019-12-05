f590c7270c36446633d67b1ecd135dcd205fd50f
Gerd Hoffmann <kraxel@redhat.com>
2019-12-05 09:53:10 +0100

    drop i686 fedora, add centos 8 stream

--- run-all.sh
+++ run-all.sh
@@ -28,14 +28,13 @@ sirius)
 	# rebuild images
 	export IMAGEFISH_DESTDIR="/vmdisk/hdd/imagefish"
 
-	linux32 ./Fedora-efi-systemd.sh	30	|| exit 1
-
-	./RHEL7-efi.sh			7.6	|| exit 1
-	./RHEL7-efi.sh			7.7	|| exit 1
-	./RHEL8-efi.sh			8.0.0	|| exit 1
-	./RHEL8-efi.sh			8.1.0	|| exit 1
+	./RHEL7-efi.sh		7.6		|| exit 1
+	./RHEL7-efi.sh		7.7		|| exit 1
+	./RHEL8-efi.sh		8.0.0		|| exit 1
+	./RHEL8-efi.sh		8.1.0		|| exit 1
 	./CentOS7-efi.sh			|| exit 1
-	./CentOS8-efi.sh			|| exit 1
+	./CentOS8-efi.sh	8		|| exit 1
+	./CentOS8-efi.sh	8-stream	|| exit 1
 	;;
 
 *)
