74b6205f03ecd85a7cc4d5443479bacdf56f8075
Gerd Hoffmann <kraxel@redhat.com>
2020-11-06 13:29:10 +0100

    rhel update

--- run-all.sh
+++ run-all.sh
@@ -25,14 +25,8 @@ fedora)
 sirius)
 	# rebuild images
 	export IMAGEFISH_DESTDIR="/vmdisk/hdd/imagefish"
-
-	./RHEL7-efi.sh		7.6		|| exit 1
-	./RHEL7-efi.sh		7.7		|| exit 1
-	./RHEL8-efi.sh		8.0.0		|| exit 1
-	./RHEL8-efi.sh		8.1.0		|| exit 1
-	./CentOS7-efi.sh			|| exit 1
-	./CentOS8-efi.sh	8		|| exit 1
-	./CentOS8-efi.sh	8-stream	|| exit 1
+	./RHEL7-efi.sh		7.8		|| exit 1
+	./RHEL8-efi.sh		8.2		|| exit 1
 	;;
 
 *)
