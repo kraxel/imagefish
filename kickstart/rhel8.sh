977a5c1ecb115e604bbeba7408f723d47e078ebc
Gerd Hoffmann <kraxel@redhat.com>
2021-10-18 12:29:13 +0200

    rhel8 tweaks

--- kickstart/rhel8.sh
+++ kickstart/rhel8.sh
@@ -2,9 +2,13 @@
 
 # config
 name="rhel"
-base="http://spunk.home.kraxel.org/mirror/rhel/redhat/rhel-8/rel-eng/RHEL-8"
 dest="/vmdisk/hdd/imagefish"
-vers="8.3.0 8.2.0"
+
+#base="http://spunk.home.kraxel.org/mirror/rhel/redhat/rhel-8/rel-eng/RHEL-8"
+#vers="8.3.0 8.2.0"
+
+base="http://sirius.home.kraxel.org/rhel-8/rel-eng/RHEL-8"
+vers="8.5.0 8.4.0"
 
 for v in $vers; do
 	disk="${dest}/${name}-${v}-ks-x86_64.qcow2"
