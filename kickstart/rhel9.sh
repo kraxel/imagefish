6ca24242822a37efb5ed2573f484d6b8cad5a023
Gerd Hoffmann <kraxel@redhat.com>
2022-07-10 19:58:15 +0200

    reorder el9 cfgs

--- kickstart/rhel9.sh
+++ kickstart/rhel9.sh
@@ -10,11 +10,11 @@ vers="9.0.0"
 for v in $vers; do
 	repo="${base}/latest-RHEL-${v}/compose/BaseOS/x86_64/os/"
 
-	disk="${dest}/${name}-${v}-ks-x86_64.qcow2"
-	../scripts/run-kickstart-install.sh "$disk" "$repo" el9.ks
-	sudo chown kraxel.kraxel "$disk"
-
 	disk="${dest}/${name}-efi-${v}-ks-x86_64.qcow2"
 	../scripts/run-kickstart-install.sh "$disk" "$repo" el9-efi.ks
+	sudo chown kraxel.kraxel "$disk"
+
+	disk="${dest}/${name}-${v}-ks-x86_64.qcow2"
+	../scripts/run-kickstart-install.sh "$disk" "$repo" el9.ks
 	sudo chown kraxel.kraxel "$disk"
 done
